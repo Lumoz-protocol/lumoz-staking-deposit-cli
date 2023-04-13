146a855b62b490d4bdc5b126e3fb6ede3795199f
Surou <cppfuns@gmail.com>
2023-04-13 15:35:02 +0800

    update# demo

--- validator_keys/make-host.sh
+++ validator_keys/make-host.sh
@@ -2,6 +2,30 @@ rm host.ini
 i=1
 while [ $i -le 100 ]
 do
-    echo "validator${i} ansible_ssh_host=172.18.28.71 externalIp='172.18.28.71' beaconRpcProvider='172.18.28.71:4000' suggestedFeeRecipient='0942737e33b1AD9B028bb4FAb46677B1e5371D79' keystoreNum=1 keystoreFile=${i} path='/mnt/md0/devnet/prysm/validators/${i}' service=validator1 grpcGatewayPort=7$(printf "%0.3d" $i)" >> host.ini
+    echo "validator${i} ansible_ssh_host=172.18.28.71 externalIp='172.18.28.71' beaconRpcProvider='172.18.28.71:4000' suggestedFeeRecipient='0942737e33b1AD9B028bb4FAb46677B1e5371D79' keystoreFile=${i} path='/mnt/md0/devnet/prysm/validators/${i}' service=validator${i} grpcGatewayPort=7$(printf "%0.3d" $i)" >> host.ini
+    i=$(($i+1))
+done
+
+while [ $i -le 200 ]
+do
+    echo "validator${i} ansible_ssh_host=172.18.28.81 externalIp='172.18.18.30' beaconRpcProvider='172.18.28.81:4000' suggestedFeeRecipient='0942737e33b1AD9B028bb4FAb46677B1e5371D79' keystoreFile=${i} path='/mnt/md0/devnet/prysm/validators/${i}' service=validator${i} grpcGatewayPort=7$(printf "%0.3d" $i)" >> host.ini
+    i=$(($i+1))
+done
+
+while [ $i -le 300 ]
+do
+    echo "validator${i} ansible_ssh_host=172.18.18.10 externalIp='172.18.18.30' beaconRpcProvider='172.18.18.10:4000' suggestedFeeRecipient='0942737e33b1AD9B028bb4FAb46677B1e5371D79' keystoreFile=${i} path='/mnt/md0/devnet/prysm/validators/${i}' service=validator${i} grpcGatewayPort=7$(printf "%0.3d" $i)" >> host.ini
+    i=$(($i+1))
+done
+
+while [ $i -le 400 ]
+do
+    echo "validator${i} ansible_ssh_host=172.18.18.20 externalIp='172.18.18.30' beaconRpcProvider='172.18.18.20:4000' suggestedFeeRecipient='0942737e33b1AD9B028bb4FAb46677B1e5371D79' keystoreFile=${i} path='/mnt/md0/devnet/prysm/validators/${i}' service=validator${i} grpcGatewayPort=7$(printf "%0.3d" $i)" >> host.ini
+    i=$(($i+1))
+done
+
+while [ $i -le 499 ]
+do
+    echo "validator${i} ansible_ssh_host=172.18.18.30 externalIp='172.18.18.30' beaconRpcProvider='172.18.18.30:4000' suggestedFeeRecipient='0942737e33b1AD9B028bb4FAb46677B1e5371D79' keystoreFile=${i} path='/mnt/md0/devnet/prysm/validators/${i}' service=validator${i} grpcGatewayPort=7$(printf "%0.3d" $i)" >> host.ini
     i=$(($i+1))
 done
